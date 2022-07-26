a47cb2927171e97716a81069d0aad08b6f48e423
sovisrushain <sovisrushain@gmail.com>
2022-07-27 00:05:21 +0530

    Implement sign up

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,6 +1,28 @@
+import './signup.css'
+
 const SignUp = () => {
     return(
-        <h1>SignUp Form</h1>
+        <div className="text-center">
+        <main className="form-signin">
+            <form>
+                <h1 className="h3 mb-3 fw-normal">Get Started With Your Account</h1>
+                <div className="form-floating mb-2">
+                    <input type="text" className="form-control" id="floatingInput" placeholder="username" />
+                    <label htmlFor="floatingInput">UserName</label>
+                </div>
+                <div className="form-floating mb-2">
+                    <input type="email" className="form-control" id="floatingInput" placeholder="email" />
+                    <label htmlFor="floatingEmail">email</label>
+                </div>
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
+                <p className="mt-5 mb-3 text-muted">&copy; 2022</p>
+            </form>
+        </main>
+    </div>
     )
 }
 

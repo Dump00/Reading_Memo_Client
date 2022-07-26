8f2cfdf8abfabbe3ec0f4e2b2f3376f12f98804f
sovisrushain <sovisrushain@gmail.com>
2022-07-27 00:06:38 +0530

    update navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,11 +4,8 @@ const NavBar = () => {
     return(
   <header className="blog-header py-3">
     <div className="row flex-nowrap justify-content-between align-items-center">
-      <div className="col-8 text-center">
+      <div className="text-center">
         <h2 className="header-title">Reading Memo</h2>
-      </div>
-      <div className="col-2 justify-content-end align-items-center">
-        <a className="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
       </div>
     </div>
   </header>
